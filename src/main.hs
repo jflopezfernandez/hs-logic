69f3ad9a2815ccb504cd34cc96852ea6e1e475c2
lopezfjose <jflopezfernandez@gmail.com>
2018-11-14 16:52:23 -0500

    Need to fix 'listRemoveFirstElement'

--- src/main.hs
+++ src/main.hs
@@ -23,10 +23,19 @@ listMax (x:xs) = max x (listMax xs)
 printListMax :: (Integral a, Show a) => [a] -> IO ()
 printListMax l = print $ listMax l
 
+listRemoveFirstElement :: Integral a => a -> [a] -> [a]
+listRemoveFirstElement _ [] = error "Empty list"
+listRemoveFirstElement m [n] | m == n    = []
+                             | otherwise = [n]
+listRemoveFirstElement m (x:xs) | m == x = (xs)
+                                | otherwise = listRemoveFirstElement m (xs)
+
 l1 :: Integral a => [a]
 l1 = [1,2,3,4,5,6,5,4,3,2,1]
 
 
 main :: IO ()
 main = do
-    printListMax l1
+    print $ l1
+    let l2 = (listRemoveFirstElement 3 l1)
+    print $ l2
